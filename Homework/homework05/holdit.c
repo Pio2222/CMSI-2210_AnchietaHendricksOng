afb9b2f7539c1eec051f5113c07bacb6d52ed3dd
Pio2222 <pongante@lion.lmu.edu>
2025-11-13 15:17:33 -0800

    Create holdit.c

---
+++ Homework/homework05/holdit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    printf("This program will time how long you can hold your breath.\n");
+    printf("Take a deep breath, then press the 'Enter' key.\n");
+    printf("When you absolutely have to exhale, press the 'Enter' key again.\n");
+    printf("The duration will be displayed in minutes and seconds.\n\n");
+
+    printf("Press 'Enter' to start...");
+    getchar();
+    time_t start_time = time(NULL);
+
+    printf("Press 'Enter' when you need to exhale.\n");
+    getchar();
+    time_t end_time = time(NULL);
+
+    double duration = difftime(end_time, start_time);
+    int minutes = (int)duration / 60;
+    int seconds = (int)duration % 60;
+
+    printf("You held your breath for %d minute(s) and %d second(s).\n", minutes, seconds);
+    return 0;
+}
