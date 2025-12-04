06386ff8a8bda1dad8c12b9c4898d5bca696153e
Ryan Hendricks <rhendri4@lion.lmu.edu>
2025-12-04 10:49:14 -0800

    Update makeNBOC.c

--- Homework/homework06/makeNBOC.c
+++ Homework/homework06/makeNBOC.c
@@ -7,13 +7,10 @@ int is_little_endian() {
     unsigned char *ptr = (unsigned char*)&x;
 
     if (*ptr == 0x78) {
-        printf("Little-endian\n");
         return 1;
     } else if (*ptr == 0x12) {
-        printf("Big-endian\n");
         return 0;
     } else {
-        printf("Unknown endianness\n");
         return -1;
     }
 }
