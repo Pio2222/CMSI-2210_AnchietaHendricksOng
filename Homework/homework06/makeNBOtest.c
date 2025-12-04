97865589228bbdb099f359070e8f824a4430e0e0
Ryan Hendricks <rhendri4@lion.lmu.edu>
2025-12-04 11:03:16 -0800

    Update makeNBOtest.c

--- Homework/homework06/makeNBOtest.c
+++ Homework/homework06/makeNBOtest.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "makeNBOC.h"
 
+int main() {
+    uint32_t test_values[] = {
+        0x12345678,
+        0xAB23EF34,
+        0x0BEEEF04,
+        0xDEADB65F,
+        0x87654321
+    };
+    int test_size = sizeof(test_values) / sizeof(test_values[0]);
+
+    for (int i = 0; i < test_size; i++) {
+        printf("0x%08X\n",makeNBOC(test_values[i]));
+    }
+
+    return 0;
+}
